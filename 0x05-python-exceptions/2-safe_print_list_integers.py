f0d7ce9ee233df03a87ed144dd6f2c7d5ee5758e
Mecreal <annoncecbh@gmail.com>
2023-12-18 06:11:53 +0100

    2-safe_print

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    while i < x:
+        try:
+            if type(my_list[i]) is not int:
+                i += 1
+                continue
+            print(my_list[i],end="")
+            count += 1
+            i += 1
+        except:
+            break
+    print("")
+    return i
