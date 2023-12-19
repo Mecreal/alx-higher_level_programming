bb434f9fb7f7f4ae3629572d487a8c38a1af4cc9
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-12-19 06:45:39 +0100

    deleting the space

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        for j in matrix[i]:
-            print("{:d}".format(j), end=" ")
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j < len(matrix[i]) - 1:
+                print(" ", end="")
         print()
