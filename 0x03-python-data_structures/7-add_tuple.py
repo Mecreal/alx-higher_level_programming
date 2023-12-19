15831049484667de02c843c2f7c7a23b4b6f9be5
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-12-19 07:05:16 +0100

    unpacking only the 2 elements

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,11 +5,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
         x_a = tuple_a[0]
         y_a = 0
     else:
-        x_a, y_a = tuple_a
+        x_a, y_a = tuple_a[:2]
     if len(tuple_b) < 2:
         x_b = tuple_b[0]
         y_b = 0
     else:
-        x_b, y_b = tuple_a
+        x_b, y_b = tuple_a[:2]
     tuple_c = (x_a + x_b, y_a + y_b)
     return tuple_c
