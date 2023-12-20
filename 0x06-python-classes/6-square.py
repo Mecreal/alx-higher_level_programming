33229e8ba8a0c727f3850c42ead21e8d47bee4d4
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-12-20 02:25:42 +0100

    changin the tests

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,8 +10,8 @@ class Square:
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0. """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         return self.__size**2
