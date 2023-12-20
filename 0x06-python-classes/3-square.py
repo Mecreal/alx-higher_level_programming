c81b424761f73d7678686774423d1a90683ec6a8
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-12-20 01:04:34 +0100

    3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Square class defines a square and performs size validation. """
+
+
+class Square:
+    """ Square class defines a square and performs size validation. """
+    def __init__(self, size=0):
+        """ Initializes a Square instance.
+        Args: size (int, optional): The size of the square (default is 0).
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0. """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+            def area(self):
+                return self.__size
+
