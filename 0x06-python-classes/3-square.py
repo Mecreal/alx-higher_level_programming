d923a2cd30f265f6824e0e5d06d4db0d854d231b
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2023-12-20 01:07:23 +0100

    3-square_corrected

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,5 +17,5 @@ class Square:
         else:
             self.__size = size
 
-            def area(self):
-                return self.__size
+    def area(self):
+        return self.__size**2       
