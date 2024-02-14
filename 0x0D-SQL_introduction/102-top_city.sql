09a8511f6d8b8c2204831efe523fc0be3ac7a4d0
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-02-14 05:26:41 +0100

    102 103

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- AVERAGE TEMPERATURE TOP 3--
 
-SELECT city, avg(value) as avg_temp from temperatures group by city ORDER BY avg_temp DESC Limit 3;
+SELECT city, avg(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- Max TEMPERATURE TOP 3--
 
-SELECT city, max(value) as max_temp from temperatures group by city ORDER BY max_temp DESC Limit 3;
+SELECT city, max(value) AS max_temp FROM temperatures GROUP BY city ORDER BY max_temp DESC LIMIT 3;
