2f152c9addd9bec4ae8980176e255ad2019360b4
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-02-14 04:37:28 +0100

    correcting the alias

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- Max TEMPERATURE TOP 3--
 
-SELECT city, max(value) as max_value from temperatures group by city ORDER BY max_value DESC Limit 3;
+SELECT city, max(value) as max_temp from temperatures group by city ORDER BY max_value DESC Limit 3;
