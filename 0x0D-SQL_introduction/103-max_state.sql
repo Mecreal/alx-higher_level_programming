9ac808e67c8fc4ac885f05d0172e2493df4b89cf
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-02-14 05:46:31 +0100

    103

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- Max TEMPERATURE TOP 3--
 
-SELECT city, max(value) AS max_temp FROM temperatures GROUP BY `state` ORDER BY `state`;
+SELECT state, max(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
