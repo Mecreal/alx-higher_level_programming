f94a6ce85c0b5772e89ff85776d556afb2f5c4fb
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-02-14 05:29:14 +0100

    102 - 103 They should clarify the tasks -_-

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- AVERAGE TEMPERATURE TOP 3--
 
-SELECT city, avg(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, avg(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- Max TEMPERATURE TOP 3--
 
-SELECT city, max(value) AS max_temp FROM temperatures GROUP BY city ORDER BY max_temp DESC LIMIT 3;
+SELECT city, max(value) AS max_temp FROM temperatures GROUP BY `state` ORDER BY `state`;
