28d15f024e76013e8fd735506f5de6fbfc9976e0
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-02-21 01:47:45 +0100

    2 corrected

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,8 +4,7 @@ CREATE DATABASE
 CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
-GRANT IF EXISTS SELECT
+GRANT SELECT
    ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
+   TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
