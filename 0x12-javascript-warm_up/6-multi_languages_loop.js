e1537f8b095d4f8d1f21445e52e579a75b196bff
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-03-12 03:55:19 +0100

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//  script that prints 3 lines: (like 1-multi_languages.js)
+// but by using an array of string and a loop
+
+const mylist = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+mylist.forEach(msg => {
+  console.log(msg);
+});
