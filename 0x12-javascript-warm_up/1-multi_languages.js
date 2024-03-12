6b0fabd66cbb9d2b5957d51ff2dcab1576e729de
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-03-12 02:49:19 +0100

    checking the error

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 /* printing in tree lines**/
 
-const myVar = 'C is fun \nPython is cool\nJavaScript is amazing';
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
 console.log(myVar);
