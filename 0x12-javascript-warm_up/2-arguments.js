012a3a9e420464a489d201ec53ac12d29de6d5d7
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-03-12 03:38:07 +0100

    correcting 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 // Script that check if the args are found or not
 
-const { argv } = require('node:process');
-
-if (argv.length < 3) {
+const args = process.argv;
+if (args.length === 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
-	console.log('Argument found');
+} else if (args.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
-  }
+  console.log('Arguments found');
+}
