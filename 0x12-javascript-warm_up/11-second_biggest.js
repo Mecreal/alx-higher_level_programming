b4f926a69d65d1eba7f4abe78abc9cf4be4b32a5
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-03-13 02:30:39 +0100

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// Search the second biggest
+
+const { argv } = require('node:process');
+
+if (argv.length <= 3) {
+  console.log(0);
+} else {
+  const l = argv.sort();
+  console.log(l.reverse()[1]);
+}
