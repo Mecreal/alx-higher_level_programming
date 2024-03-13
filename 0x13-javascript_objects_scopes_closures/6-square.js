c6e806d14d7271674f3cd8bdb05abf457c8d12bd
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-03-14 00:28:56 +0100

    square tasks

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// inherit Square from square
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.height));
+      }
+    } else {
+      super.print();
+    }
+  }
+};
