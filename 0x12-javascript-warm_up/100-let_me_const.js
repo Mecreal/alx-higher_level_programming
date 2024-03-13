e96d572d07b7b6042aad86f9c8ba93c377153d5f
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-03-13 14:41:52 +0100

    13 100 101 tasks

---
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// Modify the value of the variable in the required file
+
+myVar = 333;
--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // export the addition of 2 integers.
 
-let add = function  (a, b) {
+const add = function (a, b) {
   return a + b;
 };
-module.exports = {add}
+module.exports = { add };
