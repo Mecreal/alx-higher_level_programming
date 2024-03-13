4a630c8d99d377921c48b82c39da9e7ff0614e0b
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-03-13 23:57:44 +0100

    5 changing the ESINT

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // inherit Square from Rectangle
 
-import Rectangle from './4-rectangle';
+const Rectangle = require('./4-rectangle');
 
-export default class Square extends Rectangle {
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
