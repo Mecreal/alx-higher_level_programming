f6f8d84f3c0c73fc6b39940a418184794f6dc78d
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-03-13 01:54:33 +0100

    task 7-8

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-import { argv } from 'node:process';
+// argv2 times C is fun
+
+const { argv } = require('node:process');
 
 const number = parseInt(argv[2], 10);
 if (isNaN(number)) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${number}`);
+  console.log('Missing number of occurrences');
+} else if (number > 0) {
+  for (let i = 0; i < number; i++) {
+    console.log('C is fun');
+  }
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// a squar of argv2 times #
+
+const { argv } = require('node:process');
+
+const number = parseInt(argv[2], 10);
+if (isNaN(number)) {
+  console.log('Missing size');
+} else if (number > 0) {
+  for (let i = 0; i < number; i++) {
+      console.log('X'.repeat(number));
+  }
+}
