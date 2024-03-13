42e818a523fdc89c410cefce07eb3230ebf20292
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-03-13 01:34:19 +0100

    I will kill myself 'cause of ALX checks system

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+import { argv } from 'node:process';
+
+const number = parseInt(argv[2], 10);
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${number}`);
+}
