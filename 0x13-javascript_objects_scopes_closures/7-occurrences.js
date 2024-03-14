b821aca071e84ba9c72c1872151d275994c112ec
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-03-14 03:15:38 +0100

    task 7 done!

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// function that returns the number of occurrences in a list
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  list.forEach(el => {
+    if (searchElement === el) {
+      count++;
+    }
+  });
+  return (count);
+};
