7b1597aabc93064afd132359c3c82b894eca0dc3
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-04-29 04:59:58 +0100

    0_CORRECT

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 """
 Module to use the DB
 """
+import MySQLdb
+import sys
 
-db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3])
 
-cur = db.cursor()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
 
-cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur = db.cursor()
 
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
+    cur.execute("SELECT * FROM states")
 
-# Close Cursor and Database Connection
-cur.close()
-db.close()
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
