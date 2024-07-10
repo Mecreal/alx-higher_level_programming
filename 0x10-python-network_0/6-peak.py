0240cee905faa7b5ed7523965a83bbc4566a01d3
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-07-10 03:18:20 +0100

    modify the python code

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+This module provides a function find_peak that finds a peak in a list of
+unsorted integers.
+"""
+
 def find_peak(list_of_integers):
     """Find a peak in a list of unsorted integers."""
     if not list_of_integers:
